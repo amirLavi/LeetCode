6bd37d0b8e0d46c958e026334196e1380e180bcf
amirLavi <amir.lavi89@gmail.com>
2023-11-06 15:36:57 -0500

    Create FindMissingObservations.py

---
+++ FindMissingObservations.py
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/find-missing-observations/description/
+def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+    mean_remainder = (mean * (len(rolls) + n)) - sum(rolls)
+    res = []
+
+    if mean_remainder < n or mean_remainder > n * 6:
+        return []
+
+    while mean_remainder:
+        dice = min(mean_remainder - n + 1, 6)
+        res.append(dice)
+        n -=1
+        mean_remainder -= dice
+    return res
